cf8e49c5cae9ac0e15cf6f965f9a934843039bfe
dinyudin203 <sung031128@korea.ac.kr>
2025-11-01 05:53:41 +0000

    add bash file for setup

---
+++ setup.sh
@@ -0,0 +1,5 @@
+conda create -y -n lerobot python=3.10
+conda activate lerobot
+pip install -e .
+pip install torch==2.6.0 torchvision==0.21.0 torchcodec==0.2.1
+pip install datasets==3.4.1 huggingface-hub==0.29.2 draccus==0.10.0 diffusers==0.32.2
